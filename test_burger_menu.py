dcda09307110d8f393501cdfe24ada4abbe0e797
irina <irinafrutiz@gmail.com>
2024-03-29 18:07:49 +0100

    change: added new test

--- test_burger_menu.py
+++ test_burger_menu.py
@@ -1,11 +1,15 @@
+import time
+
+from selenium.common import NoSuchElementException
+
 from test_auth import user_auth, BASE_URL
-from test_basket import BUTTON_ADD_TO_CART, ITEMS_IN_THE_BASKET
+from test_basket import BUTTON_ADD_TO_CART, ITEMS_IN_THE_BASKET, BASKET, BASKET_ITEMS
 
 BUTTON_MENU = ('xpath', '//button[text()="Open Menu"]')
 BUTTON_LOGOUT = ('xpath', '//*[@id="logout_sidebar_link"]')
 BUTTON_ABOUT = ('xpath', '//*[@id="about_sidebar_link"]')
 BUTTON_RESET = ('xpath', '//*[@id="reset_sidebar_link"]')
-BUTTON_CLOSE = ('xpath', '(//button)[1]')
+BUTTON_CLOSE = ('xpath', '//button[text()="Close Menu"]')
 
 
 # Выход из системы
@@ -26,10 +30,23 @@ def test_about(browser, user_auth):
 
 
 # Проверка работоспособности кнопки "Reset App State"
+def test_reset_sidebar_link_basket(browser, user_auth):
+    browser.find_element(*BUTTON_ADD_TO_CART).click()
+    browser.find_element(*BASKET).click()
+    browser.find_element(*BUTTON_MENU).click()
+    browser.find_element(*BUTTON_RESET).click()
+    browser.refresh()
+    items_in_the_basket = browser.find_elements(*ITEMS_IN_THE_BASKET)
+    assert len(items_in_the_basket) == 0, "Basket is not empty"
+
+
 def test_reset_sidebar_link(browser, user_auth):
     browser.find_element(*BUTTON_ADD_TO_CART).click()
     browser.find_element(*BUTTON_MENU).click()
     browser.find_element(*BUTTON_RESET).click()
     browser.find_element(*BUTTON_CLOSE).click()
-    items_in_the_basket = browser.find_elements(*ITEMS_IN_THE_BASKET)
-    assert len(items_in_the_basket) == 0, "Basket is not empty"
+    try:
+        items_in_the_basket = browser.find_element(*BASKET_ITEMS).text
+        assert items_in_the_basket == 0, "Basket is not empty"
+    except NoSuchElementException:
+        pass
