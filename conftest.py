4eaf251c0eb30ae28896cfd374ea014e38f17591
irina <irinafrutiz@gmail.com>
2024-04-01 19:35:13 +0200

    add: user_auth to conftest.py

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from locators import Login, URL
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -8,5 +9,14 @@ def browser():
     chrome_options.add_argument("--headless")
     browser = webdriver.Chrome(options=chrome_options)
     browser.implicitly_wait(5)
+
     yield browser
     browser.quit()
+
+
+@pytest.fixture(scope="function")
+def user_auth(browser):
+    browser.get(URL.BASE_URL)
+    browser.find_element(*Login.USER_NAME).send_keys('standard_user')
+    browser.find_element(*Login.USER_PASSWORD).send_keys('secret_sauce')
+    browser.find_element(*Login.BUTTON_LOGIN).click()
