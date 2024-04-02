c712a5432ebe9d788f47dae6a6fde635ebbb68d2
irina <irinafrutiz@gmail.com>
2024-04-02 14:21:56 +0200

    add: tests with wrong user data and without user data

--- test_auth.py
+++ test_auth.py
@@ -32,3 +32,31 @@ def test_auth_negative_wrong_username(browser):
         'Wrong Error text on the site'
     assert browser.current_url == URL.BASE_URL, 'Browser go to another URL'
     assert login_button.get_attribute("value") == "LOGIN", "There is no LOGIN button"
+
+
+def test_auth_negative_wrong_user_data(browser):
+    browser.get(URL.BASE_URL)
+    browser.find_element(*Login.USER_NAME).send_keys('a')
+    browser.find_element(*Login.USER_PASSWORD).send_keys('b')
+    login_button = browser.find_element(*Login.BUTTON_LOGIN)
+    login_button.click()
+
+    error_text = browser.find_element(*Login.LOGIN_ERROR).text
+
+    assert error_text == 'Epic sadface: Username and password do not match any user in this service', \
+        'Wrong Error text on the site'
+    assert browser.current_url == URL.BASE_URL, 'Browser go to another URL'
+    assert login_button.get_attribute("value") == "LOGIN", "There is no LOGIN button"
+
+
+def test_auth_negative_without_user_data(browser):
+    browser.get(URL.BASE_URL)
+    login_button = browser.find_element(*Login.BUTTON_LOGIN)
+    login_button.click()
+
+    error_text = browser.find_element(*Login.LOGIN_ERROR).text
+
+    assert error_text == 'Epic sadface: Username is required', \
+        'Wrong Error text on the site'
+    assert browser.current_url == URL.BASE_URL, 'Browser go to another URL'
+    assert login_button.get_attribute("value") == "LOGIN", "There is no LOGIN button"
